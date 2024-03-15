8a2292575c134ead5e5c37a0bc14196a0fa92510
Sven Roeseler <sven@svero.dev>
2024-03-15 08:23:08 +0100

    Initial commit

---
+++ src/main/java/dev/svero/playground/helloworld/Main.java
@@ -0,0 +1,17 @@
+package dev.svero.playground.helloworld;
+
+/**
+ * Implements the entry point for running this Java application.
+ *
+ * @author Sven Röseler
+ */
+public class Main {
+    /**
+     * The entry point.
+     *
+     * @param args String array with all command-line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Greetings from Java!");
+    }
+}
